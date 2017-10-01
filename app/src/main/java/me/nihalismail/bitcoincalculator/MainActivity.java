05585c1ed6b938b8a812eac8d4a8fb1935b79f1a
Nihal Ismail <Nihal Ismail>
2017-10-01 22:34:24 +0530

    TextChange Listener added

--- app/src/main/java/me/nihalismail/bitcoincalculator/MainActivity.java
+++ app/src/main/java/me/nihalismail/bitcoincalculator/MainActivity.java
@@ -2,6 +2,9 @@ package me.nihalismail.bitcoincalculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
@@ -13,9 +16,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText quantityInput = (EditText) findViewById(R.id.quantityInput);
+        quantityInput.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                        calculateProfit();
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
     }
 
-    public void calculateProfit(View view)
+    public void calculateProfit()
     {
         //Declaration Stuff
 
@@ -25,19 +46,40 @@ public class MainActivity extends AppCompatActivity {
         TextView profitText = (TextView) findViewById(R.id.profitInput);
 
 
-        float sellingPrice,buyingPrice,quanity,amountOfBitcoin,profit;
+        float sellingPrice,buyingPrice,quantity,amountOfBitcoin,profit;
 
         //Getting Input from EditText
+        if(!TextUtils.isEmpty(sellingPriceInput.getText().toString())) {
+            sellingPrice = Float.parseFloat(sellingPriceInput.getText().toString());
+        }
+        else
+        {
+            sellingPrice=0;
+        }
 
-        sellingPrice=Float.parseFloat(sellingPriceInput.getText().toString());
-        buyingPrice=Float.parseFloat(buyingPriceInput.getText().toString());
-        quanity=Float.parseFloat(quantityInput.getText().toString());
+        if(!TextUtils.isEmpty(buyingPriceInput.getText().toString())) {
+            buyingPrice = Float.parseFloat(buyingPriceInput.getText().toString());
+        }
+        else
+        {
+            buyingPrice=0;
+        }
+
+        if(!TextUtils.isEmpty(quantityInput.getText().toString())) {
+            quantity = Float.parseFloat(quantityInput.getText().toString());
+        }
+        else
+        {
+            quantity=0;
+        }
 
         //Logic Stuff
-        amountOfBitcoin=quanity/buyingPrice;
-        profit=(sellingPrice*amountOfBitcoin)-(buyingPrice*amountOfBitcoin);
+        if(buyingPrice!=0) {
+            amountOfBitcoin = quantity / buyingPrice;
+            profit = (sellingPrice * amountOfBitcoin) - (buyingPrice * amountOfBitcoin);
 
-        //Display part
-        profitText.setText(String.valueOf(profit));
+            //Display part
+            profitText.setText(String.valueOf(profit));
+        }
     }
 }
