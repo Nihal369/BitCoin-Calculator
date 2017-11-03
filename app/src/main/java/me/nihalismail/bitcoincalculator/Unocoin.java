4c1a2ef55699f87030e3af6cc34aa93e2cf6910c
nihal369 <nihalismailk@gmail.com>
2017-11-03 19:30:31 +0530

    Added Unocoin and Koinex support

--- app/src/main/java/me/nihalismail/bitcoincalculator/Unocoin.java
+++ app/src/main/java/me/nihalismail/bitcoincalculator/Unocoin.java
@@ -5,11 +5,10 @@ import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextUtils;
 import android.text.TextWatcher;
-import android.util.Log;
-import android.view.View;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 public class Unocoin extends AppCompatActivity {
@@ -24,7 +23,8 @@ public class Unocoin extends AppCompatActivity {
         EditText sellingPriceInput = (EditText) findViewById(R.id.sellingPriceInput);
         EditText buyingPriceInput = (EditText) findViewById(R.id.buyingPriceInput);
         CheckBox netBanking=(CheckBox) findViewById(R.id.netBanking);
-
+        final RadioGroup radioGroup=(RadioGroup)findViewById(R.id.radioGroup);
+        final int selectedPlatform=radioGroup.getCheckedRadioButtonId();
 
 
         //Listeners
@@ -36,7 +36,7 @@ public class Unocoin extends AppCompatActivity {
 
             @Override
             public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-                        calculateProfit();
+                calculateProfit(selectedPlatform);
             }
 
             @Override
@@ -53,7 +53,7 @@ public class Unocoin extends AppCompatActivity {
 
             @Override
             public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-                calculateProfit();
+                calculateProfit(selectedPlatform);
             }
 
             @Override
@@ -70,7 +70,7 @@ public class Unocoin extends AppCompatActivity {
 
             @Override
             public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-                calculateProfit();
+                calculateProfit(selectedPlatform);
             }
 
             @Override
@@ -83,79 +83,111 @@ public class Unocoin extends AppCompatActivity {
         netBanking.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-                calculateProfit();
+                calculateProfit(selectedPlatform);
+            }
+        });
+
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                calculateProfit(radioGroup.getCheckedRadioButtonId());
             }
         });
     }
 
-    public void calculateProfit()
+    public void calculateProfit(int selectedPlatform)
     {
-        //Declaration Stuff
-
         EditText sellingPriceInput = (EditText) findViewById(R.id.sellingPriceInput);
         EditText buyingPriceInput = (EditText) findViewById(R.id.buyingPriceInput);
         EditText quantityInput = (EditText) findViewById(R.id.quantityInput);
         TextView profitText = (TextView) findViewById(R.id.profitInput);
         TextView taxText = (TextView) findViewById(R.id.taxText);
-        CheckBox netBanking=(CheckBox) findViewById(R.id.netBanking);
+        CheckBox netBanking = (CheckBox) findViewById(R.id.netBanking);
 
 
-        float sellingPrice,buyingPrice,quantity,amountOfBitcoin,profit,tax;
+        float sellingPrice, buyingPrice, quantity, amountOfBitcoin, profit, tax;
 
         //Getting Input from EditText
-        if(!TextUtils.isEmpty(sellingPriceInput.getText().toString())) {
+        if (!TextUtils.isEmpty(sellingPriceInput.getText().toString())) {
             sellingPrice = Float.parseFloat(sellingPriceInput.getText().toString());
-        }
-        else
-        {
-            sellingPrice=0;
+        } else {
+            sellingPrice = 0;
         }
 
-        if(!TextUtils.isEmpty(buyingPriceInput.getText().toString())) {
+        if (!TextUtils.isEmpty(buyingPriceInput.getText().toString())) {
             buyingPrice = Float.parseFloat(buyingPriceInput.getText().toString());
-        }
-        else
-        {
-            buyingPrice=0;
+        } else {
+            buyingPrice = 0;
         }
 
-        if(!TextUtils.isEmpty(quantityInput.getText().toString())) {
+        if (!TextUtils.isEmpty(quantityInput.getText().toString())) {
             quantity = Float.parseFloat(quantityInput.getText().toString());
+        } else {
+            quantity = 0;
         }
-        else
-        {
-            quantity=0;
-        }
+
 
         //Logic Stuff
-        if(buyingPrice!=0) {
-            amountOfBitcoin = quantity / buyingPrice;
-            profit = (sellingPrice * amountOfBitcoin) - (buyingPrice * amountOfBitcoin);
+        if(selectedPlatform==R.id.unocoinRadio)
+        {
 
-            //Unocoin fee of 1% and GST on the 1% at 18%
-            tax=(quantity*(1.0f/100.0f));
-            tax+=(tax*(18.0f/100.0f));
+            if (buyingPrice != 0) {
+                amountOfBitcoin = quantity / buyingPrice;
+                profit = (sellingPrice * amountOfBitcoin) - (buyingPrice * amountOfBitcoin);
 
-            //Selling fee
-            tax*=2;
-            //tax+=(quantity*(1.0f/100.0f));
-            //tax+=(tax*(18.0f/100.0f));
+                //Unocoin fee of 1% and GST on the 1% at 18%
+                tax = (quantity * (1.0f / 100.0f));
+                tax += (tax * (18.0f / 100.0f));
+
+                //Selling fee
+                tax *= 2;
 
 
-            //Netbanking to buy coins instantly costs nearly 2% extra
-            if(netBanking.isChecked()) {
-                tax += (quantity * (1.9f / 100.0f));
-                tax += (tax * (9.0f / 100.0f));
+                //Netbanking to buy coins instantly costs nearly 2% extra
+                if (netBanking.isChecked()) {
+                    tax += (quantity * (2.0f / 100.0f));
+                    tax += (tax * (9.0f / 100.0f));
+                }
+
+                //Display final tax
+                taxText.setText(String.valueOf(tax));
+
+                //Actual profit received by the user
+                profit -= tax;
+
+                //Display profit
+                profitText.setText(String.valueOf(profit));
             }
+        }
 
-            //Display final tax
-            taxText.setText(String.valueOf(tax));
+        else
+        {
+            if (buyingPrice != 0) {
+                amountOfBitcoin = quantity / buyingPrice;
+                profit = (sellingPrice * amountOfBitcoin) - (buyingPrice * amountOfBitcoin);
 
-            //Actual profit received by the user
-            profit-=tax;
+                //Koinex fee of 0.25%
+                tax = (quantity * (0.25f / 100.0f));
 
-            //Display profit
-            profitText.setText(String.valueOf(profit));
+                //Koinex sell fee free till 2018 JAN 1
+                //Selling fee
+                //tax *= 2;
+
+
+                //Netbanking to buy coins instantly costs 2% extra
+                if (netBanking.isChecked()) {
+                    tax += (quantity * (2 / 100.0f));
+                }
+
+                //Display final tax
+                taxText.setText(String.valueOf(tax));
+
+                //Actual profit received by the user
+                profit -= tax;
+
+                //Display profit
+                profitText.setText(String.valueOf(profit));
+            }
         }
     }
 }
